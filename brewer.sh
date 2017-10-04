d29f7b4c3fe495f4d1c3aad058ad7ef1b38c9056
Niek Beernink <n.beernink@oxilion.nl>
2017-10-04 15:05:55 +0200

    don't pass multiple --with-default-names to brew

--- brewer.sh
+++ brewer.sh
@@ -3,53 +3,52 @@
 if brew ls --versions > /dev/null ; then
 	brew update
 	brew upgrade
-	brew install \
-		ack \
-		bash \
-		bash-completion2 \
-		colordiff \
-		coreutils \
-		curl \
-		diff-so-fancy \
-		diffutils \
-		dos2unix \
-		ed --with-default-names \
-		file-formula \
-		findutils --with-default-names \
-		gawk \
-		gist \
-		git \
-		git-review \
-		gnu-indent --with-default-names \
-		gnu-sed --with-default-names \
-		gnu-tar --with-default-names \
-		gnu-which --with-default-names \
-		gnutls \
-		goaccess \
-		grep --with-default-names \
-		gzip \
-		ipcalc \
-		lastpass-cli \
-		less \
-		mtr \
-		ncdu \
-		nmap \
-		parallel \
-		pv \
-		pwgen \
-		python3 \
-		rsync \
-		screen \
-		shellcheck \
-		ssh-copy-id \
-		swaks \
-		tig \
-		tree \
-		unrar \
-		unzip \
-		vim --override-system-vi \
-		watch \
-		wget
+	brew install ack
+	brew install bash
+	brew install bash-completion2
+	brew install colordiff
+	brew install coreutils
+	brew install curl
+	brew install diff-so-fancy
+	brew install diffutils
+	brew install dos2unix
+	brew install ed --with-default-names
+	brew install file-formula
+	brew install findutils --with-default-names
+	brew install gawk
+	brew install gist
+	brew install git
+	brew install git-review
+	brew install gnu-indent --with-default-names
+	brew install gnu-sed --with-default-names
+	brew install gnu-tar --with-default-names
+	brew install gnu-which --with-default-names
+	brew install gnutls
+	brew install goaccess
+	brew install grep --with-default-names
+	brew install gzip
+	brew install ipcalc
+	brew install lastpass-cli
+	brew install less
+	brew install mtr
+	brew install ncdu
+	brew install nmap
+	brew install parallel
+	brew install pv
+	brew install pwgen
+	brew install python3
+	brew install rsync
+	brew install screen
+	brew install shellcheck
+	brew install ssh-copy-id
+	brew install swaks
+	brew install tig
+	brew install tree
+	brew install unrar
+	brew install unzip
+	brew install vim --override-system-vi
+	brew install watch
+	brew install wget
 	brew cask install caskroom/fonts/font-hack
 else
 	echo "Install brew first!"
