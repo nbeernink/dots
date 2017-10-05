fa9a84cc350d27a8d0d7b63c84689e94ebf73668
Niek Beernink <n.beernink@oxilion.nl>
2017-10-05 23:57:17 +0200

    refactor and use symlinks for easy git updates
    inspired by the awesome https://github.com/holman/dotfiles.git
    fixed some shell issues in the bootstrap.sh script with shellcheck

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ A collection of my personal dotfiles and tools I frequently use.
 
 Feel free to fork this project and use what you like.
 
-1. git clone this repo
-2. run `bootstrap.sh` to rsync the dotfiles into your home directory
-3. run `brewer.sh` to install tools via osx' brew
+1. git clone https://github.com/nbeernink/dots.git ~/git/dots
+2. `cd ~/git/dots`
+3. execute `bootstrap.sh`
+4. run `brewer.sh` to install tools via osx' brew
+
+# Thanks
+https://github.com/holman/dotfiles for inspiration and parts of code
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,127 @@
 #!/bin/bash
+#
+# bootstrap your homedir with your favorite dots
+#
 
-echo "Pull updates..."
-git pull origin master;
+cd "$(dirname "$0")" || exit 1
+dotsdir=$(pwd -P)
 
-function deploy() {
-	#rsync dotfiles into homedir
-	rsync -av --no-perms \
-		--exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude "bootstrap.sh" \
-		--exclude "brewer.sh" \
-		--exclude "README.md" \
-		. ~;
-	source ~/.bash_profile && echo "Deployed dotfiles!"
+#set status messages
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
+}
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
+}
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  echo ''
+  exit
 }
 
-if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-	deploy;
-else
-	read -rp "Overwrite files in your home directory? y/n "
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		deploy;
-	fi;
-fi;
-unset deploy;
+setup_gitconfig () {
+  if ! [ -f git/gitconfig.local.symlink ]
+  then
+    info 'setup gitconfig'
+
+    user ' - What is your github author name?'
+    read -re git_authorname
+    user ' - What is your github author email?'
+    read -re git_authoremail
+    pwd
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
+
+    success 'gitconfig'
+  fi
+}
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite='' backup='' skip=''
+  local action=''
+
+  if [ -f "$dst" ] || [ -d "$dst" ] || [ -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      local currentSrc
+      currentSrc="$(readlink "$dst")"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -rn 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+}
+install_dotfiles () {
+  info 'installing dotfiles'
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  for src in $(find -H "$dotsdir" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  do
+    dst="$HOME/.$(basename "${src%.*}")"
+    link_file "$src" "$dst"
+  done
+}
+setup_gitconfig
+install_dotfiles
